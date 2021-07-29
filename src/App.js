8cb95be51e712e87d2981ec65637aec5a0e590a0
qqri <50269335+qqri@users.noreply.github.com>
2021-07-29 14:04:30 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import './dist/css/style.css';
-import './dist/css/pages/dashboard1.css';
-import 'bootstrap/dist/css/bootstrap.css'
 
 class App extends Component {
   render () {
